575d2fa3357d7a0f3c953713cd336100df4a6a6e
laktam abdellah <laktamlaktamabdellah@gmail.com>
2023-08-30 02:45:21 +0100

    invitation from p2 to p2

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,49 @@ import { Grid } from "@mui/material";
 import "./App.css";
 import { useEffect, useState } from "react";
 import { Box } from "./components/Box";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3001"); // Replace with your server URL
 
 function App() {
   // const [player, setPlayer] = useState(true);
   const [board, setBoard] = useState([]);
-  const [lobby, setLobby] = useState({ p1: "", p2: "", lobbyId: 0 });
-
+  const [player, setPlayer] = useState("");
+  const [other, setOther] = useState("");
   useEffect(() => {
-    const p1 = window.prompt("Please enter your username:");
-    if (p1)
-      setLobby((prev) => ({
-        ...prev, // Spread the existing properties
-        p1, // Update the player property
-      }));
-    const p2 = window.prompt("Please enter the other player username:");
-    if (p2)
-      setLobby((prev) => ({
-        ...prev, // Spread the existing properties
-        p2, // Update the player property
-      }));
+    //get id from server
+    socket.on("id", (data) => {
+      console.log("Received id:", data);
+      setPlayer(data);
+    });
 
+    socket.on("invitation", (data) => {
+      console.log("invitation from ", data);
+    });
+    //   setLobby((prev) => ({
+    //     ...prev, // Spread the existing properties
+    //     p1: p1, // Update the player property
+    //   }));
     //need to generate unique lobby id for different games
-    setLobby((prev) => ({
-      ...prev, // Spread the existing properties
-      lobbyId: 100, // Update the player property
-    }));
-    
+    // setLobby((prev) => ({
+    //   ...prev, // Spread the existing properties
+    //   id: 100, // Update the player property
+    // }));
+    //send lobby data to server
+    //change this to make first player invite second player
+    //then create the lobby if he accepts
+    // socket.emit("createLobby", { p1, p2, id: 100 });
   }, []);
+
+  const invitePlayer = () => {
+    const p2 = window.prompt("Please enter player id:");
+    if (p2) setOther(p2);
+    socket.emit("invitePlayer", { p1: player, p2: p2 });
+  };
+
   return (
     <div className="App">
+      <button onClick={invitePlayer}>invite a player</button>
       <Grid container>
         <Grid item xs={0} sm={1.5} md={3} />
         {/*  */}
--- src/Server.js
+++ src/Server.js
@@ -1,22 +1,36 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // Adjust this to match your client's domain
+    methods: ["GET", "POST"],
+  },
+});
 const PORT = process.env.PORT || 3001;
-
+const games = {};
 // Handle WebSocket connections
-io.on('connection', (socket) => {
-  console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected :", socket.id);
+  socket.emit("id", socket.id);
 
-  // Add your socket event listeners here
-  // For example: socket.on('event', (data) => { ... });
-  
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  //invite a player
+  socket.on("invitePlayer", (data) => {
+    console.log(`player ${data.p1} invited ${data.p2}`);
+    io.to(data.p2).emit(`invitation`, data.p1);
+  });
+
+  socket.on("createLobby", (lobby) => {
+    console.log("Lobby info: ", lobby);
+    games[lobby.id] = { p1: lobby.p1, p2: lobby.p2, board: [] };
+    console.log("Games ", games);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
 
