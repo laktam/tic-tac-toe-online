5e3f00c7507d2bded0993a6bd380bb28fbdc5e03
laktam abdellah <laktamlaktamabdellah@gmail.com>
2023-09-03 14:42:02 +0100

    update index.html

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,10 @@ import Board from "../src/images/CropedCleanedGrayBoard.png";
 import { BackDrop } from "./components/BackDrop";
 
 // const socket = io("http://localhost:3001"); // Replace with your server URL
-const socket = io("http://tic-tac-toe-express.eu-4.evennode.com/"); // Replace with your server URL
+// const socket = io("http://tic-tac-toe-express.eu-4.evennode.com/"); // Replace with your server URL
+const socket = io(
+  "https://cors-anywhere.herokuapp.com/http://tic-tac-toe-express.eu-4.evennode.com/"
+);
 
 function App() {
   // const [player, setPlayer] = useState(true);
